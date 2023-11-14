0753fdaf7e70eb1fd522c97e13b870be41f784b9
Pedro H. Mota <110642021+mota-pedro@users.noreply.github.com>
2023-11-14 16:30:36 -0300

    feat: timer implementado

---
+++ pomodoro.js
@@ -0,0 +1,38 @@
+let timerInterval;
+let minutes = 0;
+let seconds = 0;
+
+document.getElementById("start-button").addEventListener("click", startTimer);
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+    seconds++;
+
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+
+        if (minutes == 25) {
+            clearInterval(timerInterval);
+            changeStButtonText("Descanso");
+        }
+    }
+
+    const formattedTime = formatTime(minutes, seconds);
+    document.getElementById("timer").innerText = formattedTime;
+}
+
+function formatTime(minutes, seconds) {
+    return (padZero(minutes) + ':' + padZero(seconds));
+}
+
+function padZero(value) {
+    return value < 10 ? '0' + value : value;
+}
+
+function changeStButtonText(text) {
+    document.getElementById("start-button").innerText = text;
+}
